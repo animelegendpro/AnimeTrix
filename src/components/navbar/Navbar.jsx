b9d74ce42e6d5d8718adafde0923a576dca559cf
animelegendpro <167015173+animelegendpro@users.noreply.github.com>
2025-02-24 02:19:55 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -62,7 +62,7 @@ function Navbar() {
               <img
           src="https://i.postimg.cc/tgC5ntmZ/20250224-014956.png"
           alt={logoTitle}
-          className="w-[100px] h-[100px]"
+          className="w-[1280px] h-[312px]"
         />
       </div> 
           <WebSearch />
